9a1d9bb28b9e9e65686d37c0f2ecd4484b567dc5
ee <ee@163.com>
2022-07-02 03:25:18 +0800

    feat: 增加tp5的valet驱动

---
+++ README.md
@@ -0,0 +1,11 @@
+#  Valet ThinkPHP5 Driver
+
+## 简介
+> 一个为了访问 thinkphp5 应用的 valet 驱动
+> 而且针对 fastadmin 应用进行对应的优化
+
+## 使用
+> 拷贝 ThinkPHP5ValetDriver.php 文件到 valet 驱动文件夹 ~/.config/valet/Drivers
+
+## License
+> The Lumen framework is open-sourced software licensed under the MIT license
---
+++ ThinkPHP5ValetDriver.php
@@ -0,0 +1,63 @@
+<?php
+
+class ThinkPHP5ValetDriver extends ValetDriver
+{
+
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath . '/public/index.php') &&
+            file_exists($sitePath . '/think');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+
+        if (
+            file_exists($staticFilePath = $sitePath . '/public' . $uri)
+            && is_file($staticFilePath)
+        ) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        if (isset($_SERVER['HTTP_X_ORIGINAL_HOST'], $_SERVER['HTTP_X_FORWARDED_HOST'])) {
+            $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+        }
+
+        if (empty($_SERVER['PATH_INFO']) && !is_file($sitePath . '/public' . $uri)) {
+            $_SERVER['PATH_INFO'] = $uri;
+            $_SERVER['SCRIPT_NAME'] = '/index.php';
+        }
+        $_SERVER['SCRIPT_FILENAME'] = $sitePath . '/public' . $_SERVER['SCRIPT_NAME'];
+
+        return $_SERVER['SCRIPT_FILENAME'];
+    }
+}
